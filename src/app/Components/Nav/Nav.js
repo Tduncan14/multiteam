e6c8947e34120e25021c1ce066bf902a7c12c11d
tduncan14 <nbatre1@yahoo.com>
2026-02-08 13:20:35 -0600

    keep moving forward

---
+++ src/app/Components/Nav/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import './style.css';
+
+const Nav = () => {
+    return (
+        <header className="headContainer">
+            <div className="leftHead">
+                <div className="logo">
+                    <Image src="/assets/logo.svg" height={150} width={150} alt="this is the logo" />
+                </div>
+
+                <div className="leftNav">
+                    <Link href="/">Home</Link>
+
+                    <Link href="/about">About</Link>
+                </div>
+            </div>
+            {/* this is the end of the left side */}
+
+
+            <div className="rightHead">
+
+                <button>
+                    <Link href="/contact">Contact</Link>
+                </button>
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Nav
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center  font-sans dark:bg-black">
+
+
+
+    </div>
+  );
+}
