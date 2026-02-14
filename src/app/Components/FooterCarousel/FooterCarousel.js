fa2909b59c8176d44b04ab919d2265faffb22c5d
tduncan14 <nbatre1@yahoo.com>
2026-02-14 17:01:29 -0600

    keep moving forward

---
+++ src/app/Components/FooterCarousel/FooterCarousel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './footerCarosel.css'
+
+const FooterCarousel = () => {
+    return (
+        <div className="footerCarousel">
+
+            <h1> Ready to get started?</h1>
+
+
+
+            <button> contact us</button>
+
+        </div>
+    )
+}
+
+export default FooterCarousel
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import './home.css'
+import FooterCarousel from "./Components/FooterCarousel/FooterCarousel";
 
 export default function Home() {
   return (
@@ -99,12 +100,73 @@ export default function Home() {
       {/* end of midHeader */}
 
 
-      <section className="homeHeaderEnd">
+      <section className="homeEnd">
+
+        <div className="titleName">
+          <h1> Delivering real results for top companies.</h1>
+          <h1>Some of our <span>success stories.</span></h1>
+        </div>
+
+
+
+        <div className="gallerySuccess">
+
+          <div className="successStory">
+
+            <p>"The team perfectly fit the specialized skill set required.
+              They focused on the most essential features helping us launch the platform
+              eight months faster than planned
+            </p>
+
+            <h3>Kady Baker</h3>
+            <h4>Product Manager at Bookmark </h4>
+
+
+            <img src="/assets/avatar-kady.jpg" />
+          </div>
+
+
+          <div className="successStory">
+
+            <p>"We needed to automate our entire onboarding process.
+              The came in and built out the whole journey. Since going, user retention
+              has gone through the roof!"
+            </p>
+
+            <h3>Kady Baker</h3>
+            <h4>Founder of Manage </h4>
+
+
+            <img src="/assets/avatar-aiysha.jpg" />
+          </div>
+
+
+          <div className="successStory">
+
+            <p>"Amazing. Our team helped us build an app that delivered
+              a new experience for hiring a physio.
+              The launch was an instant success with 100k
+              downloads in the first month.""
+            </p>
+
+            <h3>Arthur Clarke</h3>
+            <h4>Co-founder of MyPhysia</h4>
+
+
+            <img src="/assets/avatar-arthur.jpg" />
+          </div>
+
+        </div>
+
 
 
 
 
       </section>
+
+
+
+      <FooterCarousel />
 
 
 
