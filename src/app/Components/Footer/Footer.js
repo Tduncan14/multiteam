934b78c9f87f27ca49e660f0d156b9c734f3abe2
tduncan14 <nbatre1@yahoo.com>
2026-02-16 03:14:10 -0600

    keep moving forward

--- src/app/Components/Footer/Footer.js
+++ src/app/Components/Footer/Footer.js
@@ -1,11 +1,74 @@
 import React from 'react'
 import './Footer.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
         <div className="footerContainer">
-            this footer
+
+            <div className="footLeft">
+
+                <div className="footLeftLeft">
+
+
+                    <Link href="/"> <div className="logo">
+                        <Image src="/assets/logo.svg" height={150} width={150} alt="this is the logo" />
+                    </div>
+                    </Link>
+
+                    <div className="footerLeftnav">
+                        <ul>
+                            <li><button>home</button></li>
+                            <li><button>about</button></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className="footLeftRight">
+
+                    <ul>
+                        <li>987 Hillcrest Lane</li>
+                        <li>Irvine,CA</li>
+                        <li>California 92714</li>
+                        <li>Call Us:949-833-7432</li>
+                    </ul>
+
+
+                </div>
+
+
+            </div>
+
+
+            <div className="footRight">
+
+                <div className="footerSocialIconsGallery">
+
+                    <img src="/assets/icon-facebook.svg" />
+                    <img src="/assets/icon-pinterest.svg" />
+                    <img src="/assets/icon-twitter.svg" />
+
+
+
+
+                </div>
+
+
+                <p>Copyright 2026. All Rights Reserved</p>
+
+
+
+            </div>
+
+
         </div>
+
+
+
+
+
     )
 }
 
