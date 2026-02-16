674dad1d0248cfa6cae89698ec7ffa976b6e99f0
tduncan14 <nbatre1@yahoo.com>
2026-02-16 05:11:51 -0600

    keep moving forward

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,88 +1,115 @@
 import React from 'react'
 import './about.css'
+import FooterCarousel from '../Components/FooterCarousel/FooterCarousel'
 
 const About = () => {
     return (
-        <div className="aboutContainer">
-            <div className="upperAbout">
+        <>
+            <div className="aboutContainer">
+                <div className="upperAbout">
 
-                <div className="upperAboutLeft">
-                    <h1> About</h1>
+                    <div className="upperAboutLeft">
+                        <h1> About</h1>
+
+                    </div>
+
+
+                    <div className="upperAboutRight">
+
+                        <p> We help companies build dynamic teams made up of top global talent.
+                            Using our network of passionate professionals we drive innovation and
+                            deliver incredible outcomes. Talented, diverse teams shaped the best products
+                            and experiences. We'll bring those teams to you.
+                        </p>
+
+                    </div>
+
+
 
                 </div>
 
+                {/* end of the upperAbout */}
 
-                <div className="upperAboutRight">
+                {/*  this is  the beginning of the director's page */}
 
-                    <p> We help companies build dynamic teams made up of top global talent.
-                        Using our network of passionate professionals we drive innovation and
-                        deliver incredible outcomes. Talented, diverse teams shaped the best products
-                        and experiences. We'll bring those teams to you.
-                    </p>
+                <div className="directorsContainer">
+
+                    <h1> Meet the directors</h1>
+
+
+                    <div className="directorsGalleries">
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-nikita.jpg' />
+                            <h1> Nikita Marks</h1>
+                            <h3>Founder & CEO</h3>
+                        </div>
+
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-christian.jpg' />
+                            <h1> Cristian Duncan</h1>
+                            <h3>Co-founder & COO</h3>
+                        </div>
+
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-cruz.jpg' />
+                            <h1> Cruz Hamer</h1>
+                            <h3>Co-founder & CTO </h3>
+                        </div>
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-drake.jpg' />
+                            <h1> Drake Heaton</h1>
+                            <h3>Business Development Lead</h3>
+                        </div>
+
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-griffin.jpg' />
+                            <h1> Griffin Wise</h1>
+                            <h3>Lead Marketing </h3>
+                        </div>
+
+
+                        <div className="directorGallery">
+                            <img src='/assets/avatar-aden.jpg' />
+                            <h1> Aden Allan</h1>
+                            <h3>Lead Marketing</h3>
+                        </div>
+
+
+
+
+                    </div>
 
                 </div>
+
+                {/* end of the directorsContainer */}
+
+                <div className="clientsContainer">
+                    <h1> Some of our clients</h1>
+
+                    <div className="clientsName">
+
+                        <ul>
+                            <li><img src="/assets/logo-the-verge.png" /></li>
+                            <li><img src="/assets/logo-jakarta-post.png" /></li>
+                            <li><img src="/assets/logo-the-guardian.png" /></li>
+                            <li><img src="/assets/logo-tech-radar.png" /></li>
+                            <li><img src="/assets/logo-gadgets-now.png" /></li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/* end of the clientsContainer */}
+
+
 
             </div>
-
-            {/* end of the upperAbout */}
-
-            {/*  this is  the beginning of the director's page */}
-
-            <div className="directorsContainer">
-
-                <h1> Meet the directors</h1>
-
-
-                <div className="directorsGalleries">
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-nikita.jpg' />
-                        <h1> Nikita Marks</h1>
-                        <h3>Founder & CEO</h3>
-                    </div>
-
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-christian.jpg' />
-                        <h1> Cristian Duncan</h1>
-                        <h3>Co-founder & COO</h3>
-                    </div>
-
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-cruz.jpg' />
-                        <h1> Cruz Hamer</h1>
-                        <h3>Co-founder & CTO </h3>
-                    </div>
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-drake.jpg' />
-                        <h1> Drake Heaton</h1>
-                        <h3>Business Development Lead</h3>
-                    </div>
-
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-griffin.jpg' />
-                        <h1> Griffin Wise</h1>
-                        <h3>Lead Marketing </h3>
-                    </div>
-
-
-                    <div className="directorGallery">
-                        <img src='/assets/avatar-aden.jpg' />
-                        <h1> Aden Allan</h1>
-                        <h3>Lead Marketing</h3>
-                    </div>
-
-
-
-
-                </div>
-
-            </div>
-
-        </div>
+            <FooterCarousel />
+        </>
     )
 }
 
